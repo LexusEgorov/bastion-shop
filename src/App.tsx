46a273ccfa6cdfef0eb9d62cfbe74acf38dff7ab
lexusegorov <alexey.egorov.job@gmail.com>
2022-07-18 15:08:23 +0700

    Компонент подвал

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/footer/footer';
 import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import './styles/footer.css'
+
+function Footer() : JSX.Element{
+  return (
+    <footer className="page-footer">
+      <div className='wide-container'></div>
+    </footer>
+  );
+}
+
+export default Footer;
